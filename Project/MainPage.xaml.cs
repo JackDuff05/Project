28306cd0ac9dc25add316f53416e7c859766bd5f
Jack Duff <G00466452@atu.ie>
2025-12-14 16:27:30 +0000

    Today i added the car with an image i generated using pixel lab, i also added buttons that allow the car to move between lanes

--- Project/MainPage.xaml.cs
+++ Project/MainPage.xaml.cs
@@ -2,30 +2,63 @@
 
 public partial class MainPage : ContentPage
 {
-    
     private RoadDrawing myRoad;
+
     private IDispatcherTimer animationTimer;
+
     private bool isAnimating = false;
+
+    private int currentLane = 1;
+
+    private const double LANE_SPACING = 100.0; 
 
     public MainPage()
     {
         InitializeComponent();
-        
+
         myRoad = new RoadDrawing();
-        
-        RoadCanvas.Drawable = myRoad;       
+
+        RoadCanvas.Drawable = myRoad;
+
         animationTimer = Dispatcher.CreateTimer();
         animationTimer.Interval = TimeSpan.FromMilliseconds(16); 
         animationTimer.Tick += OnTimerTick;
-    
+
         isAnimating = true;
         animationTimer.Start();
+
+        UpdateCarPosition();
     }
 
     private void OnTimerTick(object sender, EventArgs e)
-    { 
+    {
         myRoad.Update();
-        
+
         RoadCanvas.Invalidate();
+    }
+
+    private void OnLeftButtonClicked(object sender, EventArgs e)
+    {
+        if (currentLane > 0)
+        {
+            currentLane--;
+            UpdateCarPosition();
+        }
+    }
+
+    private void OnRightButtonClicked(object sender, EventArgs e)
+    {
+        if (currentLane < 2)
+        {
+            currentLane++;
+            UpdateCarPosition();
+        }
+    }
+
+    private void UpdateCarPosition()
+    {
+        double horizontalOffset = (currentLane - 1) * LANE_SPACING;
+
+        PlayerCar.TranslationX = horizontalOffset;
     }
 }
--- Project/RoadDrawing.cs
+++ Project/RoadDrawing.cs
@@ -20,42 +20,43 @@ public class RoadDrawing : IDrawable
     {
         laneWidth = roadWidth / numberOfLanes;
     }
-    
+
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
-       
+
         float middleOfScreen = dirtyRect.Width / 2;
 
         float leftEdgeOfRoad = middleOfScreen - roadWidth / 2;
-      
+
         canvas.FillColor = Colors.Green.WithAlpha(0.3f);
         canvas.FillRectangle(dirtyRect);
 
         canvas.FillColor = Colors.DarkGray;
         canvas.FillRectangle(leftEdgeOfRoad, 0, roadWidth, dirtyRect.Height);
-        
+
         canvas.StrokeColor = Colors.White;
         canvas.StrokeSize = 3;
-        
+
         canvas.DrawLine(leftEdgeOfRoad, 0, leftEdgeOfRoad, dirtyRect.Height);
-        
+
         canvas.DrawLine(leftEdgeOfRoad + roadWidth, 0, leftEdgeOfRoad + roadWidth, dirtyRect.Height);
-        
+
         canvas.StrokeColor = Colors.Yellow;
         canvas.StrokeSize = 2;
-        
+
         canvas.StrokeDashPattern = new float[] { lineLength, gapLength };
 
         canvas.StrokeDashOffset = offset;
-       
+
         float firstDividerX = leftEdgeOfRoad + laneWidth;
         canvas.DrawLine(firstDividerX, 0, firstDividerX, dirtyRect.Height);
-      
+
         float secondDividerX = leftEdgeOfRoad + (laneWidth * 2);
         canvas.DrawLine(secondDividerX, 0, secondDividerX, dirtyRect.Height);
-      
+
         canvas.StrokeDashPattern = null;
     }
+
     public void Update()
     {   
         offset = offset - speed;
