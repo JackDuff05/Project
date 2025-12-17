3975c61ad3258ea65ebc68d1afbf4b7a9509f2f6
Jack Duff <G00466452@atu.ie>
2025-12-17 17:42:00 +0000

    Got the enemy cars added, i found this very challenging and my file witht the car images didnt work properly so had to add the manually.

--- Project/MainPage.xaml.cs
+++ Project/MainPage.xaml.cs
@@ -1,16 +1,23 @@
-﻿namespace Project;
+﻿using Microsoft.Maui.Dispatching;
+using Microsoft.Maui.Layouts;
+
+namespace Project;
 
 public partial class MainPage : ContentPage
 {
     private RoadDrawing myRoad;
 
     private IDispatcherTimer animationTimer;
+    private IDispatcherTimer enemySpawnTimer;
 
     private bool isAnimating = false;
 
     private int currentLane = 1;
 
     private const double LANE_SPACING = 100.0;
+
+    private List<EnemyCar> enemyCars = new List<EnemyCar>();
+    private Random random = new Random();
 
 
     public MainPage()
@@ -22,11 +29,16 @@ public partial class MainPage : ContentPage
         RoadCanvas.Drawable = myRoad;
 
         animationTimer = Dispatcher.CreateTimer();
-        animationTimer.Interval = TimeSpan.FromMilliseconds(16); 
+        animationTimer.Interval = TimeSpan.FromMilliseconds(16);
         animationTimer.Tick += OnTimerTick;
 
         isAnimating = true;
         animationTimer.Start();
+
+        enemySpawnTimer = Dispatcher.CreateTimer();
+        enemySpawnTimer.Interval = TimeSpan.FromMilliseconds(2000); 
+        enemySpawnTimer.Tick += SpawnEnemyCar;
+        enemySpawnTimer.Start();
 
         UpdateCarPosition();
     }
@@ -37,6 +49,49 @@ public partial class MainPage : ContentPage
 
         RoadCanvas.Invalidate();
 
+        
+        for (int i = enemyCars.Count - 1; i >= 0; i--)
+        {
+            enemyCars[i].Update();
+
+            if (i == 0)
+            {
+                System.Diagnostics.Debug.WriteLine($"Enemy 0 position: {enemyCars[i].yPosition}");
+            }
+
+            if (enemyCars[i].yPosition > 800)
+            {
+                GameLayout.Children.Remove(enemyCars[i].carImage);
+
+                enemyCars.RemoveAt(i);
+                System.Diagnostics.Debug.WriteLine($"Removed enemy car");
+            }
+        }
+    }
+    private void SpawnEnemyCar(object sender, EventArgs e)
+    {
+        if (isAnimating)
+        {
+            int randomLane = random.Next(0, 3); 
+
+            string[] carImages = { "bluecar.png", "greencar.png", "yellowcar.png", "policecar.png", "truckenemy.png" };
+            string randomCarImage = carImages[random.Next(carImages.Length)];
+
+            Image enemyCarImage = new Image
+            {
+                Source = randomCarImage,  
+                WidthRequest = 100,
+                HeightRequest = 150
+            };
+
+            AbsoluteLayout.SetLayoutBounds(enemyCarImage, new Rect(0, 0, 100, 150));
+            AbsoluteLayout.SetLayoutFlags(enemyCarImage, AbsoluteLayoutFlags.None);
+
+            GameLayout.Children.Add(enemyCarImage);
+
+            EnemyCar newEnemy = new EnemyCar(randomLane, enemyCarImage);
+            enemyCars.Add(newEnemy);
+        }
     }
 
     private void OnLeftButtonClicked(object sender, EventArgs e)
---
+++ Project/enemycar.cs
@@ -0,0 +1,39 @@
+﻿namespace Project;
+
+public class EnemyCar
+{
+    public float xPosition;
+    public float yPosition;
+    public int lane; 
+    public float speed = 5f;
+    public float carWidth = 100f;
+    public float carHeight = 150f;
+    public Image carImage; 
+
+    public EnemyCar(int laneNumber, Image imageElement)
+    {
+        lane = laneNumber;
+        carImage = imageElement;
+
+        if (lane == 0)
+            xPosition = 50f;   
+        else if (lane == 1)
+            xPosition = 150f; 
+        else
+            xPosition = 250f; 
+
+        yPosition = -150f;
+
+        UpdateImagePosition();
+    }
+    public void Update()
+    {
+        yPosition += speed; 
+        UpdateImagePosition();
+    }
+
+    private void UpdateImagePosition()
+    {
+        AbsoluteLayout.SetLayoutBounds(carImage, new Rect(xPosition, yPosition, carWidth, carHeight));
+    }
+}
